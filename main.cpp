f7482a94096091ceb70da343c3522d7942bf3803
ubuntu-VMware <zhangrenshan_tx@126.com>
2022-04-10 16:27:21 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@ using namespace std;
 
 void sort(int arr[],int size)
 {
-	
+    int tmp = 0;
+    for(int i = 0; i < size-1; ++i)
+    {
+        for(int j = 0; j < size-1-i; ++j)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j] = tmp;
+            }
+        }
+    }
 }
+
 
 int main()
 {
